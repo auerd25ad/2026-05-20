4543350bccafac04c0acd1ddf9d40ee1466b6b79
auerd25ad <auerd.25ad@acsjszki.hu>
2026-05-20 13:58:59 +0200

    2

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ var tomb = [];
 
 // 2. Töltsd fel a tömböt 10 darab véletlen számmal a Math.random() függvény segítségével
 for (var i = 0; i < 10; i++) {
-    tomb[i] = Math.random();
+    tomb[i] = Math.ceil(Math.random()*5);
 }
 
 // 3. Írd ki a tömb elemeit
 for (var i = 0; i < tomb.length; i++) {
     document.write(tomb[i] + "<br>");
 }
+
+// Összegzés
+var osszeg = 0;
+for (var i = 0; i < tomb.length; i++) {
+    osszeg = osszeg + tomb[i];
+}
+
+document.write(" <br> A jegyek összege: " + osszeg);
+
+// Átlagszámítás
+var atlag = osszeg / tomb.length
+document.write(" <br> Az átlagom: " + atlag)
 
