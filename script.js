c26f53fcb047b0af0bb5e8659b23926569131968
auerd25ad <auerd.25ad@acsjszki.hu>
2026-05-20 11:29:45 +0200

    1

---
+++ script.js
@@ -0,0 +1,13 @@
+// 1. Hozz létre egy üres tömböt, aminek a neve legyen tomb
+var tomb = [];
+
+// 2. Töltsd fel a tömböt 10 darab véletlen számmal a Math.random() függvény segítségével
+for (var i = 0; i < 10; i++) {
+    tomb[i] = Math.random();
+}
+
+// 3. Írd ki a tömb elemeit
+for (var i = 0; i < tomb.length; i++) {
+    document.write(tomb[i] + "<br>");
+}
+
