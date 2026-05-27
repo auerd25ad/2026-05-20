d0b476b45184df870ea347fe72f7dce39c40a817
auerd25ad <auerd.25ad@acsjszki.hu>
2026-05-27 11:30:17 +0200

    4

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-// 1. Hozz létre egy üres tömböt, aminek a neve legyen tomb
-var tomb = [];
-var jegyek = document.getElementById("jegyek");
+function fv1() {
+    // 1. Hozz létre egy üres tömböt, aminek a neve legyen tomb
+    var tomb = [];
+    var jegyek = document.getElementById("jegyek");
 
 
-// 2. Töltsd fel a tömböt 10 darab véletlen számmal a Math.random() függvény segítségével
-for (var i = 0; i < 10; i++) {
-    tomb[i] = Math.ceil(Math.random() * 5);
+    // 2. Töltsd fel a tömböt 10 darab véletlen számmal a Math.random() függvény segítségével
+    for (var i = 0; i < 10; i++) {
+        tomb[i] = Math.ceil(Math.random() * 5);
+    }
+
+    // 3. Írd ki a tömb elemeit
+    for (var i = 0; i < tomb.length; i++) {
+        //document.write(tomb[i] + "<br>");
+        jegyek.innerHTML = jegyek.innerHTML + tomb[i] + ",";
+    }
+
+    // Összegzés
+    var osszeg = 0;
+    for (var i = 0; i < tomb.length; i++) {
+        osszeg = osszeg + tomb[i];
+    }
+
+    //document.write(" <br> A jegyek összege: " + osszeg);
+    document.getElementById("jegyekOsszege").innerHTML = osszeg;
+
+    // Átlagszámítás
+    //var atlag = osszeg / tomb.length
+    //document.write(" <br> Az átlagom: " + atlag);
+    document.getElementById("jegyekAtlaga").innerHTML = osszeg / tomb.length;
 }
 
-// 3. Írd ki a tömb elemeit
-for (var i = 0; i < tomb.length; i++) {
-    //document.write(tomb[i] + "<br>");
-    jegyek.innerHTML = jegyek.innerHTML + tomb[i] + ",";
-}
 
-// Összegzés
-var osszeg = 0;
-for (var i = 0; i < tomb.length; i++) {
-    osszeg = osszeg + tomb[i];
-}
-
-//document.write(" <br> A jegyek összege: " + osszeg);
-document.getElementById("jegyekOsszege").innerHTML = osszeg;
-
-// Átlagszámítás
-//var atlag = osszeg / tomb.length
-//document.write(" <br> Az átlagom: " + atlag);
-document.getElementById("jegyekAtlaga").innerHTML = osszeg / tomb.length;
 
